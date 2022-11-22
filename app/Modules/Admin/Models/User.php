8a88b53cea1feeac61cd7d2a597db4a7dbc7c678
Ahmed AlMoselhy <ah.almoselhy@gmail.com>
2022-11-22 13:38:56 +0200

    modify user model

--- app/Modules/Admin/Models/User.php
+++ app/Modules/Admin/Models/User.php
@@ -2,10 +2,48 @@
 
 namespace App\Modules\Admin\Models;
 
+use App\Bll\Constants;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function getImageAttribute($value): ?string
+    {
+        return $value ? asset(Constants::UserPath . $this->id . DIRECTORY_SEPARATOR . $value) : null;
+    }
 }
