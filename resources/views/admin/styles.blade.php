f6772054089e73186ec6d8d5e1a905a2a0b181f8
Ahmed AlMoselhy <ah.almoselhy@gmail.com>
2022-11-22 16:32:42 +0200

    load style & scripts

---
+++ resources/views/admin/styles.blade.php
@@ -0,0 +1,37 @@
+<link rel="apple-touch-icon" href="{{ asset('app-assets/images/ico/apple-icon-120.png') }}">
+<link rel="shortcut icon" type="image/x-icon" href="{{ 'images/logo.png' }}">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
+    rel="stylesheet">
+
+<!-- BEGIN: Vendor CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/vendors' . $dir . '.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/charts/apexcharts.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/extensions/toastr.min.css') }}">
+<!-- END: Vendor CSS-->
+
+<!-- BEGIN: Theme CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/bootstrap.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/bootstrap-extended.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/colors.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/components.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/themes/dark-layout.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/themes/bordered-layout.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/themes/semi-dark-layout.css') }}">
+<!-- END: Theme CSS-->
+
+<!-- BEGIN: Page CSS-->
+<link rel="stylesheet" type="text/css"
+    href="{{ asset('app-assets/css' . $dir . '/core/menu/menu-types/vertical-menu.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/pages/authentication.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/pages/dashboard-ecommerce.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/plugins/charts/chart-apex.css') }}">
+<link rel="stylesheet" type="text/css"
+    href="{{ asset('app-assets/css' . $dir . '/plugins/extensions/ext-component-toastr.css') }}">
+<!-- END: Page CSS-->
+
+<!-- BEGIN: Custom CSS-->
+@if ($dir == '-rtl')
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css' . $dir . '/custom' . $dir . '.css') }}">
+@endif
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/assets/css/style' . $dir . '.css') }}">
+<!-- END: Custom CSS-->
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,19 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@php
+    $dir = '';
+    if (request()->is('admin/*') || request()->is('admin')) {
+        $dir = \App\Bll\Lang::getAdminLangDir();
+    } else {
+        $dir = \App\Bll\Lang::getLangDir();
+    }
+    if ($dir == 'rtl') {
+        $dir = '-rtl';
+    } else {
+        $dir = '';
+    }
+@endphp
+
+<html lang="{{ \App\Bll\Lang::getAdminLangCode() }}" data-textdirection="{{ $dir }}">
 
 <head>
     <meta charset="utf-8">
@@ -10,12 +24,13 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-    @include('global.styles')
+    @include('global.styles', ['dir' => $dir])
 
     @if (request()->is('admin/*') || request()->is('admin'))
-        @include('admin.styles')
+        @include('admin.styles', ['dir' => $dir])
+        {{-- @include('admin.meta') --}}
     @else
-        @include('home.styles')
+        @include('home.styles', ['dir' => $dir])
     @endif
 
     <!-- Scripts -->
@@ -27,13 +42,14 @@
 <body class="font-sans antialiased">
     @inertia
 
-    @include('global.scripts')
+    @include('global.scripts', ['dir' => $dir])
 
-    @if (request()->is('admin/*'))
-        @include('admin.scripts')
+    @if (request()->is('admin/*') || request()->is('admin'))
+        @include('admin.scripts', ['dir' => $dir])
     @else
-        @include('home.scripts')
+        @include('home.scripts', ['dir' => $dir])
     @endif
+
 </body>
 
 </html>
